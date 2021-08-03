e1b985a92821b7d7337d73d7d8db86f4d8267178
Nimit Patel <nimit.patel@powertakeoff.com>
2021-08-03 13:14:58 -0400

    Changed path server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/demo-deploy'));
+app.use(express.static(__dirname + '/dist/usersnap-deploy'));
 app.get('/*', function(req,res) {
-   res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
+   res.sendFile(path.join(__dirname+'/dist/usersnap-deploy/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
